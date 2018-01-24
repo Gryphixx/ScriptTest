1639a6f1a911e899c08f42f1a94eba5f405298fe
Denzel Ooms <DenzelOoms@live.be>
2018-01-24 21:50:36 +0100

    Test

--- ScriptTest/ScriptTest/Program.cs
+++ ScriptTest/ScriptTest/Program.cs
@@ -11,6 +11,7 @@ namespace ScriptTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
         }
     }
 }
